086c9db1d5b8000cdad8b10825f21008aa85264d
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-04-24 14:21:45 -0300

    Basic movement and camera following player added

---
+++ ProjectMysteryChat/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    int playerSpeed;
+
+    public void MovementKeys()
+    {
+        if (Input.GetKey(InputHandler.input.walkUp))
+        {
+            transform.position += (Vector3.forward) * playerSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(InputHandler.input.walkDown))
+        {
+            transform.position -= (Vector3.forward) * playerSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(InputHandler.input.walkLeft))
+        {
+            transform.position += (Vector3.left) * playerSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(InputHandler.input.walkRight))
+        {
+            transform.position += (Vector3.right) * playerSpeed * Time.deltaTime;
+        }
+    }
+
+    public void Behave()
+    {
+        if (InputHandler.input.inputDetected())
+        {
+            MovementKeys();
+        }
+    }
+
+    public int GetPlayerSpeed()
+    {
+        return playerSpeed;
+    }
+
+    void Update()
+    {
+        Behave();
+    }
+}
