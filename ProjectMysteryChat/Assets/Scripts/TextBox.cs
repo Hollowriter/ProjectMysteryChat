aa13aa194acc71395c0ce567250ea829f8ac06a6
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-04-24 17:46:38 -0300

    starting to create the textbox

--- ProjectMysteryChat/Assets/Scripts/TextBox.cs
+++ ProjectMysteryChat/Assets/Scripts/TextBox.cs
@@ -1,18 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextBox : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    string[] _speech = new string[4] { "Hi", "How are you ?", "Nice weather today", "Want some ice cream ?" };
+    int _speechIndex = 0;
+    private void OnGUI()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GUILayout.Label(this._speech[this._speechIndex]);
+        if (GUILayout.Button("Next"))
+            this._speechIndex++; // Todavia no esta terminado
     }
 }
