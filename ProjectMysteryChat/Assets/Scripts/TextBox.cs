8d47afc9cddd3dfc39e8b2a885c07f7b1d868f20
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-04-25 18:14:45 -0300

    Escritura de textos basica. Falta pulir el codigo e insertar dialogos externamente.

--- ProjectMysteryChat/Assets/Scripts/TextBox.cs
+++ ProjectMysteryChat/Assets/Scripts/TextBox.cs
@@ -5,12 +5,47 @@ using UnityEngine.UI;
 
 public class TextBox : MonoBehaviour
 {
-    string[] _speech = new string[4] { "Hi", "How are you ?", "Nice weather today", "Want some ice cream ?" };
-    int _speechIndex = 0;
+    string[] speech = new string[4] { "Hi", "How are you ?", "Nice weather today", "Want some ice cream ?" };
+    char letter;
+    int speechIndex = 0;
+    bool textWritten = false;
+    [SerializeField]
+    Text dialogueText;
+
     private void OnGUI()
     {
-        GUILayout.Label(this._speech[this._speechIndex]);
+        if (textWritten == false)
+        {
+            StopAllCoroutines();
+            if (speechIndex < speech.Length)
+            {
+                StartCoroutine(DialogTyping(this.speech[this.speechIndex]));
+            }
+            textWritten = true;
+        }
         if (GUILayout.Button("Next"))
-            this._speechIndex++; // Todavia no esta terminado
+        {
+            if (speechIndex < speech.Length)
+            {
+                this.speechIndex++;
+            }
+            else
+            {
+                return;
+            }
+            dialogueText.text = "";
+            textWritten = false;
+        }
+    }
+
+    IEnumerator DialogTyping(string _word)
+    {
+        dialogueText.text = "";
+        foreach (char letter in _word.ToCharArray())
+        {
+            dialogueText.text += letter;
+            Debug.Log("entro");
+            yield return null;
+        }
     }
 }
