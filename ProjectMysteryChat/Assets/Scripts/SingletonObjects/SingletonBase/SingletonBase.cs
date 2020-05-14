9c15bdeb8f432da03c946c7b0d97714f5f2b04b7
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-05-14 14:57:45 -0300

    carpetas reorganizadas

---
+++ ProjectMysteryChat/Assets/Scripts/SingletonObjects/SingletonBase/SingletonBase.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonBase<T> : MonoBehaviour where T : MonoBehaviour
+{
+    bool activated = false;
+    public static T _instance;
+
+    public static T instance
+    {
+        get
+        {
+            if (_instance == null) _instance = (T)FindObjectOfType(typeof(T));
+            return _instance;
+        }
+    }
+
+    protected virtual void SingletonAwake()
+    {
+        if (_instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            _instance = this as T;
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    protected virtual void BehaveSingleton()
+    {
+    }
+
+    protected virtual bool ConditionsToBeActive()
+    {
+        return activated;
+    }
+
+    public void SetActivated(bool _activated)
+    {
+        activated = _activated;
+    }
+
+    public bool GetActivated()
+    {
+        return activated;
+    }
+}
