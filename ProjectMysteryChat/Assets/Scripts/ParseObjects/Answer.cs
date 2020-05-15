b4852315597b3351acb1cecd5bb39b8376b6d3c5
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-05-15 16:09:20 -0300

    Clase para las respuestas con evidencia empezada

---
+++ ProjectMysteryChat/Assets/Scripts/ParseObjects/Answer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Answer
+{
+    public string ExpectedAnswer;
+    public string CorrectDialog;
+    public string WrongDialog;
+}
+
+[System.Serializable]
+public class AnswerCollection
+{
+    public Answer[] Answers;
+}
