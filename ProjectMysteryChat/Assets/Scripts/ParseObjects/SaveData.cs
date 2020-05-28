de78a7d3a362ea94720a5e6b6ba8f77dfbf9b24e
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-05-28 16:18:26 -0300

    estructura hecha para guardar como json los datos

---
+++ ProjectMysteryChat/Assets/Scripts/ParseObjects/SaveData.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Stage 
+{
+    public string SceneName;
+    public string PositionX;
+    public string PositionY;
+}
+
+[System.Serializable]
+public class Interaction 
+{
+    public string InteractionName;
+}
+
+[System.Serializable]
+public class InteractionCollection 
+{
+    public Interaction[] Interactions;
+}
+
+[System.Serializable]
+public class SaveData
+{
+    public Stage StageSaved;
+    public EvidenceCollection EvidenceSaved;
+    public InteractionCollection InteractionsDone;
+}
