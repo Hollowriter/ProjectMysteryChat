a1c0dcd89d229467706c74f932b85bd72d60e935
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-06-29 13:23:14 -0300

    dialogos agregados y scripts reorganizados

---
+++ ProjectMysteryChat/Assets/Scripts/InteractObjects/SaveObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveObject : InteractObject
+{
+    void SaveData() 
+    {
+        SaveDataDocument.instance.Save();
+    }
+
+    public override void BehaveInteraction()
+    {
+        SaveData();
+    }
+}
---
+++ ProjectMysteryChat/Assets/Scripts/InteractObjects/TestObjects/ActionableTestObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionableTestObject : InteractObject
+{
+    public override void NearPlayer()
+    {
+        Debug.Log("I'm just near him");
+    }
+
+    public override void BehaveInteraction()
+    {
+        Debug.Log("I function");
+    }
+}
