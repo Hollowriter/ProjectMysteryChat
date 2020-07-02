be5066e3f956140eb032ffec0d48a655357bcf87
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-07-02 16:23:02 -0300

    Added dialogs for transport beacon

---
+++ ProjectMysteryChat/Assets/Scripts/InteractObjects/TestObjects/EvidenceObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvidenceObject : InteractObject
+{
+    private void Awake()
+    {
+        IShouldExist();
+    }
+
+    public override void NearPlayer()
+    {
+        SetTextToBox();
+    }
+
+    public override void BehaveInteraction()
+    {
+        ShowText();
+        SetPermanentInteraction();
+        SetEvidenceToInventory();
+    }
+}
