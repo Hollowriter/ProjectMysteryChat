46c91ece78bb40cb7e5829c58275aa8ade0081a7
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-07-23 15:52:47 -0300

    Creadas clases base para personajes y sus algoritmos strategy.

---
+++ ProjectMysteryChat/Assets/Scripts/SceneAlgorithms/SceneAlgorithm.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SceneAlgorithm : MonoBehaviour
+{
+    public abstract void ActScript(string[] interactionFiles, GameObject[] pointsToGo); // Note: Needs to be implemented
+}
