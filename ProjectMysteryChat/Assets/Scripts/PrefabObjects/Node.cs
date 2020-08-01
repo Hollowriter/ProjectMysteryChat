6609099c12406e728005b92abda9f0b5c7854bbc
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-08-01 15:53:02 -0300

    Node and Node Creator done

--- ProjectMysteryChat/Assets/Scripts/PrefabObjects/Node.cs
+++ ProjectMysteryChat/Assets/Scripts/PrefabObjects/Node.cs
@@ -4,5 +4,215 @@ using UnityEngine;
 
 public class Node : MonoBehaviour
 {
-    // Pendiente
+    List<Node> obj = new List<Node>();
+    float cost = 1;
+    float heuristic = 0;
+    float startPointCost = 0;
+    float goalPointCost = 0;
+    float totalCost = 0;
+    bool isOpen = false;
+    bool isClosed = false;
+    bool isDestiny = false;
+    bool isPath = false;
+    bool isBlocked = false;
+    Node theParent = null;
+    Node theChild = null;
+    Ray leftRay;
+    Ray rightRay;
+    Ray frontRay;
+    Ray backRay;
+    RaycastHit hit;
+
+    void Start()
+    {
+        leftRay = new Ray(this.gameObject.transform.position, Vector3.left);
+        rightRay = new Ray(this.gameObject.transform.position, Vector3.right);
+        frontRay = new Ray(this.gameObject.transform.position, Vector3.forward);
+        backRay = new Ray(this.gameObject.transform.position, Vector3.back);
+    }
+
+    void Update()
+    {
+        if (Physics.Raycast(leftRay, out hit, 0.15f) ||
+            Physics.Raycast(rightRay, out hit, 0.15f) ||
+            Physics.Raycast(frontRay, out hit, 0.15f) ||
+            Physics.Raycast(backRay, out hit, 0.15f))
+        {
+            if (hit.collider.tag == "block") // Nota: Revisar esta linea de codigo con respecto a objetos bloqueantes
+            {
+                isBlocked = true;
+            }
+        }
+        else
+        {
+            isBlocked = false;
+        }
+    }
+
+    public void AddAdjacent(Node n)
+    {
+        if (!obj.Contains(n))
+            obj.Add(n);
+    }
+
+    public void Reset()
+    {
+        obj.Clear();
+        cost = 1;
+        heuristic = 0;
+        startPointCost = 0;
+        goalPointCost = 0;
+        totalCost = 0;
+        isOpen = false;
+        isClosed = false;
+        isDestiny = false;
+        isPath = false;
+        theParent = null;
+        theChild = null;
+    }
+
+    public List<Node> Adjacents()
+    {
+        return obj;
+    }
+
+    public void SetOpen(bool open)
+    {
+        isOpen = open;
+    }
+
+    public void SetClosed(bool closed)
+    {
+        isClosed = closed;
+    }
+
+    public void SetDestiny(bool destiny)
+    {
+        isDestiny = destiny;
+    }
+
+    public void SetIsBlocked(bool blocking)
+    {
+        isBlocked = blocking;
+    }
+
+    public void SetParent(Node parent)
+    {
+        theParent = parent;
+        if (theParent != null)
+        {
+            SetTotalCost(theParent);
+            SetStartTotalCost(theParent);
+            theParent.SetChild(this);
+        }
+    }
+
+    public void SetChild(Node child)
+    {
+        theChild = child;
+        SetGoalTotalCost(child);
+    }
+
+    public void SetAsPath(bool sure)
+    {
+        isPath = sure;
+    }
+
+    public void SetTotalCost(Node parent)
+    {
+        if (parent == null)
+        {
+            totalCost = cost;
+        }
+        else
+        {
+            totalCost = cost + parent.totalCost;
+        }
+    }
+
+    public void SetHeuristicalTotalCost()
+    {
+        heuristic = goalPointCost + startPointCost;
+    }
+
+    public void SetGoalTotalCost(Node child)
+    {
+        if (child)
+        {
+            goalPointCost = 1 + child.GetGoalTotalCost();
+        }
+    }
+
+    public void SetStartTotalCost(Node parent)
+    {
+        if (parent)
+        {
+            startPointCost = 1 + GetParent().GetStartTotalCost();
+        }
+    }
+
+    public float GetHeuristicalTotalCost()
+    {
+        SetHeuristicalTotalCost();
+        return heuristic;
+    }
+
+    public float GetGoalTotalCost()
+    {
+        return goalPointCost;
+    }
+
+    public float GetStartTotalCost()
+    {
+        return startPointCost;
+    }
+
+    public bool GetOpen()
+    {
+        return isOpen;
+    }
+
+    public bool GetClosed()
+    {
+        return isClosed;
+    }
+
+    public bool GetDestiny()
+    {
+        return isDestiny;
+    }
+
+    public bool GetIsBlocked()
+    {
+        return isBlocked;
+    }
+
+    public bool GetAsPath()
+    {
+        return isPath;
+    }
+
+    public float GetTotalCost()
+    {
+        return totalCost;
+    }
+
+    public Node GetParent()
+    {
+        return theParent;
+    }
+
+    public Node GetChild()
+    {
+        return theChild;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (isPath == true)
+        {
+            Gizmos.color = Color.blue;
+        }
+        Gizmos.DrawCube(this.transform.position, Vector3.one);
+    }
 }
--- ProjectMysteryChat/Assets/Scripts/SingletonObjects/Pathfinding/NodeCreator.cs
+++ ProjectMysteryChat/Assets/Scripts/SingletonObjects/Pathfinding/NodeCreator.cs
@@ -4,9 +4,101 @@ using UnityEngine;
 
 public class NodeCreator : SingletonBase<NodeCreator>
 {
+    public int Cols;
+    public int Rows;
+    Node[,] nodes;
+    Node nodeOrigin = null;
+
+    void InstantiateNodes() 
+    {
+        nodes = new Node[Cols, Rows];
+        for (int row = 0; row < Rows; row++)
+        {
+            for (int col = 0; col < Cols; col++)
+            {
+                GameObject go = new GameObject("Node");
+                nodes[col, row] = go.AddComponent<Node>();
+                go.transform.position = new Vector3(col * 2.0f, 0.0f, row * 2.0f);
+            }
+        }
+    }
+
+    void CheckAdjacency() 
+    {
+        for (int row = 0; row < Rows; row++)
+        {
+            for (int col = 0; col < Cols; col++)
+            {
+                if (col > 0)
+                {
+                    nodes[col - 1, row].AddAdjacent(nodes[col, row]);
+                    nodes[col, row].AddAdjacent(nodes[col - 1, row]);
+                }
+                if (row > 0)
+                {
+                    nodes[col, row - 1].AddAdjacent(nodes[col, row]);
+                    nodes[col, row].AddAdjacent(nodes[col, row - 1]);
+                }
+                if (col < Cols - 1)
+                {
+                    nodes[col + 1, row].AddAdjacent(nodes[col, row]);
+                    nodes[col, row].AddAdjacent(nodes[col + 1, row]);
+                }
+                if (row < Rows - 1)
+                {
+                    nodes[col, row + 1].AddAdjacent(nodes[col, row]);
+                    nodes[col, row].AddAdjacent(nodes[col, row + 1]);
+                }
+            }
+        }
+    }
+
+    protected override void SingletonAwake()
+    {
+        base.SingletonAwake();
+        InstantiateNodes();
+        CheckAdjacency();
+    }
+
     private void Awake()
     {
         SingletonAwake();
     }
-    // Pendiente para el pathfinding
+
+    public void ResetAllNodes()
+    {
+        for (int row = 0; row < Rows; row++)
+        {
+            for (int col = 0; col < Cols; col++)
+            {
+                nodes[col, row].SetOpen(false);
+                nodes[col, row].SetClosed(false);
+                nodes[col, row].SetDestiny(false);
+                nodes[col, row].SetAsPath(false);
+                nodes[col, row].SetParent(null);
+                nodes[col, row].SetChild(null);
+            }
+        }
+    }
+
+    public Node GetNodeByPosition(Vector3 pos)
+    {
+        nodeOrigin = nodes[0, 0];
+        float dist = (pos - nodes[0, 0].transform.position).magnitude;
+        for (int c = 0; c < Cols; c++)
+        {
+            for (int r = 0; r < Rows; r++)
+            {
+                Node n = nodes[c, r];
+                float newDist = (pos - n.transform.position).magnitude;
+
+                if (dist > newDist)
+                {
+                    nodeOrigin = n;
+                    dist = newDist;
+                }
+            }
+        }
+        return nodeOrigin;
+    }
 }
