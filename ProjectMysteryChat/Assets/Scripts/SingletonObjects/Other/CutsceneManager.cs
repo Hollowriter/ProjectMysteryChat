b940943ebcf3997b11c43f81ff5dca0910dc381c
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-10-05 16:10:04 -0300

    nueva coleccion de escenas

--- ProjectMysteryChat/Assets/Scripts/SingletonObjects/Other/CutsceneManager.cs
+++ ProjectMysteryChat/Assets/Scripts/SingletonObjects/Other/CutsceneManager.cs
@@ -7,22 +7,27 @@ public class CutsceneManager : SingletonBase<CutsceneManager> // Cutscene manage
 {
     List<SceneAlgorithm> cutscenes; // Nota: Hacer que se puedan leer varias listas a la vez a voluntad.
     List<SceneAlgorithm> readingCutscenes; // Nota: Hacer que se lean solo las escenas de la lista.
+    int cutscenesFinished;
 
     protected override void SingletonAwake()
     {
         base.SingletonAwake();
         cutscenes = null;
         readingCutscenes = new List<SceneAlgorithm>();
+        cutscenesFinished = 0;
         SetActivated(false);
         DontDestroyOnLoad(gameObject);
     }
 
     protected override bool ConditionsToBeActive()
     {
-        if (cutscenes != null /*&& cutscene.GetAlgorithmEnd() == false*/) 
+        if (cutscenes != null) 
         {
-            SetActivated(true);
-            return GetActivated();
+            if (cutscenes.Count > 0)
+            {
+                SetActivated(true);
+                return GetActivated();
+            }
         }
         SetActivated(false);
         return GetActivated();
@@ -38,7 +43,12 @@ public class CutsceneManager : SingletonBase<CutsceneManager> // Cutscene manage
         cutscenes = _cutscenes;
     }
 
-    public void ProcessCutscenes() // Esto es el encargado de rellenar las escenas a leer. (EN PROCESO)
+    public List<SceneAlgorithm> GetCutscenes()
+    {
+        return cutscenes;
+    }
+
+    void ProcessCutscenes() // Esto es el encargado de rellenar las escenas a leer. (EN PROCESO)
     {
         if (readingCutscenes.Count == 0) 
         {
@@ -53,22 +63,61 @@ public class CutsceneManager : SingletonBase<CutsceneManager> // Cutscene manage
         }
     }
 
-    public List<SceneAlgorithm> GetCutscenes() 
+    void ReadActScript()
     {
-        return cutscenes;
+        if (readingCutscenes.Count > 0)
+        {
+            for (int i = 0; i < readingCutscenes.Count; i++)
+            {
+                readingCutscenes[i].ActScript();
+            }
+        }
     }
 
-    public void ActScript() 
+    void CheckReadingCutscenesEnding()
     {
-        if (ConditionsToBeActive()) 
+        if (readingCutscenes.Count > 0)
         {
-            
+            for (int i = 0; i < readingCutscenes.Count; i++)
+            {
+                if (readingCutscenes[i].GetAlgorithmEnd())
+                {
+                    cutscenesFinished++;
+                }
+            }
         }
+        if (cutscenesFinished >= readingCutscenes.Count)
+        {
+            readingCutscenes.Clear();
+        }
+        cutscenesFinished = 0;
+    }
+
+    void CheckCutscenesFinished() 
+    {
+        for (int i = 0; i < cutscenes.Count; i++) 
+        {
+            if (cutscenes[i].GetAlgorithmEnd()) 
+            {
+                cutscenesFinished++;
+            }
+        }
+        if (cutscenesFinished >= cutscenes.Count) 
+        {
+            cutscenes.Clear();
+        }
+        cutscenesFinished = 0;
     }
 
     protected override void BehaveSingleton()
     {
-        ActScript();
+        if (ConditionsToBeActive())
+        {
+            ProcessCutscenes();
+            ReadActScript();
+            CheckReadingCutscenesEnding();
+            CheckCutscenesFinished();
+        }
     }
 
     private void Update()
