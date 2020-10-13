a54f52207ffa822c49b54a45da6ef13f32761cc2
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-10-13 15:22:57 -0300

    las escenas se getean en lugar de tener que arrastrarse

--- ProjectMysteryChat/Assets/Scripts/InteractObjects/CutsceneObject.cs
+++ ProjectMysteryChat/Assets/Scripts/InteractObjects/CutsceneObject.cs
@@ -4,7 +4,21 @@ using UnityEngine;
 
 public class CutsceneObject : InteractObject
 {
-    public List<SceneAlgorithm> scenes;
+    List<SceneAlgorithm> scenes;
+
+    void Begin() 
+    {
+        scenes = new List<SceneAlgorithm>();
+        for (int i = 0; i < this.gameObject.GetComponentsInChildren<SceneAlgorithm>().Length; i++)
+        {
+            scenes.Add(this.gameObject.GetComponentsInChildren<SceneAlgorithm>()[i]);
+        }
+    }
+
+    private void Start()
+    {
+        Begin();
+    }
 
     public override void NearPlayer()
     {
