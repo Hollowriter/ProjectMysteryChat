07bad0c9bc75f982e63741e9564a6c3388a24391
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-10-13 16:06:36 -0300

    Empezando con los dialogos para las cutscenes

---
+++ ProjectMysteryChat/Assets/Scripts/SceneAlgorithms/DialogAlgorithm.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogAlgorithm : SceneAlgorithm // NOTA: PENDIENTE DE TERMINAR
+{
+    public string[] dialogsForTheScene;
+    int dialogsFinished;
+
+    void Begin() 
+    {
+        dialogFiles = dialogsForTheScene;
+        dialogsFinished = 0;
+    }
+
+    private void Awake()
+    {
+        Begin();
+    }
+
+    public int GetDialogsFinished() 
+    {
+        return dialogsFinished;
+    }
+
+    public override void ActScript()
+    {
+        throw new System.NotImplementedException();
+    }
+}
