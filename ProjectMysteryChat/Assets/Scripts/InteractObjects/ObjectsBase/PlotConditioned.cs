3aacf15950042a7fee9223a73ce5c3346fe35c8c
Hollowriter <deadgamergx2@hotmail.com.ar>
2020-10-21 16:18:27 -0300

    Condicionador por plot points hecho, pendiente de testear.

---
+++ ProjectMysteryChat/Assets/Scripts/InteractObjects/ObjectsBase/PlotConditioned.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlotConditioned : MonoBehaviour // NOTA: Pendiente de testear.
+{
+    [SerializeField]
+    List<string> plotPointNames;
+    [SerializeField]
+    bool toActivate;
+    int plotPointActivated;
+
+    void Begin() 
+    {
+        plotPointActivated = 0;
+    }
+
+    private void Awake()
+    {
+        Begin();
+    }
+
+    public void CheckPlotPoints() 
+    {
+        plotPointActivated = 0;
+        for (int i = 0; i < plotPointNames.Count; i++) 
+        {
+            for (int r = 0; i < PlotPointManager.instance.GetPlotPointCollection().PlotPoint.Length; r++) 
+            {
+                if (plotPointNames[i] == PlotPointManager.instance.GetPlotPointCollection().PlotPoint[r].PlotPointName) 
+                {
+                    plotPointActivated++;
+                    break;
+                }
+            }
+        }
+    }
+
+    public bool ConfirmActivation() 
+    {
+        if (plotPointActivated == plotPointNames.Count) 
+        {
+            return toActivate;
+        }
+        return !toActivate;
+    }
+}
