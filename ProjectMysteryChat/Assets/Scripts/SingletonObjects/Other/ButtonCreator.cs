8b73890886b8d8f27401c5d6f8cf94d98715cc31
Hollowriter <rodrigodiazklipphan@gmail.com>
2022-03-24 16:45:18 -0300

    Making a new buttons creator

---
+++ ProjectMysteryChat/Assets/Scripts/SingletonObjects/Other/ButtonCreator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonCreator : SingletonBase<ButtonCreator>
+{
+    [SerializeField]
+    GameObject buttonToCreate;
+    [SerializeField]
+    int sizeButtonX;
+    [SerializeField]
+    int sizeButtonY;
+
+    public void CreateButton(string text){
+        // Create the button. (Hollow)
+    }
+}
