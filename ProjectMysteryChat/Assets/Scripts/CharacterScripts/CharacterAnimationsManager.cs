c315c6bdc2debbb96d1e35b1d699071e3cb053c7
Hollowriter <rodrigodiazklipphan@gmail.com>
2022-05-12 16:22:36 -0300

    Character animations manager created and added to Maki.

---
+++ ProjectMysteryChat/Assets/Scripts/CharacterScripts/CharacterAnimationsManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationsManager : MonoBehaviour
+{
+    Animator characterAnimator;
+
+    void Start()
+    {
+        characterAnimator = this.gameObject.GetComponentInChildren<Animator>();
+    }
+
+    public void ChangeCharacterPosition(int position)
+    {
+        characterAnimator.SetInteger("Position", position);
+    }
+
+    public void SetMoving(bool isMoving)
+    {
+        characterAnimator.SetBool("Moving", isMoving);
+    }
+}
