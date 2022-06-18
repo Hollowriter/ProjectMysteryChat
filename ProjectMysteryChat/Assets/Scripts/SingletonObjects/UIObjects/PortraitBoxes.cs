32baff134e6350fcd2382eb37d8183d1ed37d008
Hollowriter <rodrigodiazklipphan@gmail.com>
2022-06-18 20:39:23 -0300

    Portrait scripts on progress

---
+++ ProjectMysteryChat/Assets/Scripts/SingletonObjects/UIObjects/PortraitBoxes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PortraitBoxes : SingletonBase<PortraitBoxes>
+{
+    [SerializeField]
+    RawImage portraitLeft;
+    [SerializeField]
+    RawImage portraitRight;
+
+    protected override void SingletonAwake()
+    {
+        base.SingletonAwake();
+        portraitLeft.gameObject.SetActive(false);
+        portraitRight.gameObject.SetActive(false);
+    }
+
+    void Awake()
+    {
+        SingletonAwake();
+    }
+}
