a357446b5c937f7191b6c4603731f9328106d714
Hollowriter <rodrigodiazklipphan@gmail.com>
2022-06-18 18:12:17 -0300

    Class for the portraits created

--- ProjectMysteryChat/Assets/Scripts/ParseObjects/Dialogs.cs
+++ ProjectMysteryChat/Assets/Scripts/ParseObjects/Dialogs.cs
@@ -4,10 +4,6 @@
 public class Dialogs
 {
     public string Text;
-    // public string character1;
-    // public string expression1;
-    // public string character2;
-    // public string expression2;
 }
 
 [System.Serializable]
---
+++ ProjectMysteryChat/Assets/Scripts/ParseObjects/Portraits.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Portraits
+{
+    public string Character1;
+    public string Expression1;
+    public string Character2;
+    public string Expression2;
+}
+
+[System.Serializable]
+public class PortraitCollection
+{
+    public Portraits[] Portraits;
+}
