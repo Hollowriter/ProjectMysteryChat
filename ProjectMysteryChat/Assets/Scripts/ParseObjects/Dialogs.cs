94700a04f2b6743685850b6b37fcd3f90f2a6d9c
Hollowriter <rodrigodiazklipphan@gmail.com>
2022-06-18 16:47:14 -0300

    Starting the portrait representation of the characters

--- ProjectMysteryChat/Assets/Scripts/ParseObjects/Dialogs.cs
+++ ProjectMysteryChat/Assets/Scripts/ParseObjects/Dialogs.cs
@@ -4,6 +4,10 @@
 public class Dialogs
 {
     public string Text;
+    // public string character1;
+    // public string expression1;
+    // public string character2;
+    // public string expression2;
 }
 
 [System.Serializable]
