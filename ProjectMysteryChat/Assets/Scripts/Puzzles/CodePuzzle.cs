9a9478f9a2f781ab698db25aa44afef1405b71d9
Hollowriter <rodrigodiazklipphan@gmail.com>
2022-09-25 17:48:03 -0300

    Making the base of code puzzles

---
+++ ProjectMysteryChat/Assets/Scripts/Puzzles/CodePuzzle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodePuzzle : MonoBehaviour
+{
+    [SerializeField]
+    List<Number> numbers;
+    [SerializeField]
+    int answer;
+
+    public virtual bool CheckAnswer()
+    {
+        return false;
+    }
+}
---
+++ ProjectMysteryChat/Assets/Scripts/Puzzles/Number.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Number : MonoBehaviour
+{
+    int _value;
+    const int LIMITVALUE = 9;
+
+    void Awake()
+    {
+        _value = 0;
+    }
+
+    public void SetValue(int value)
+    {
+        _value = value;
+    }
+
+    public int GetValue()
+    {
+        return _value;
+    }
+
+    public void IncreaseValue()
+    {
+        if (_value < LIMITVALUE)
+        {
+            _value++;
+            return;
+        }
+        _value = 0;
+    }
+
+    public void DecreaseValue()
+    {
+        if (_value >= 0)
+        {
+            _value--;
+            return;
+        }
+        _value = LIMITVALUE;
+    }
+}
