ddb2df250eba96ce1cce48031424c092fae434a6
Hollowriter <rodrigodiazklipphan@gmail.com>
2022-10-12 17:32:32 -0300

    Non intereactable animated objects affected by the plot conditioned

--- ProjectMysteryChat/Assets/Scripts/CharacterScripts/CharacterAnimationsManager.cs
+++ ProjectMysteryChat/Assets/Scripts/CharacterScripts/CharacterAnimationsManager.cs
@@ -7,12 +7,28 @@ public class CharacterAnimationsManager : MonoBehaviour
     Animator characterAnimator;
     public PositionToLook startPosition;
     string currentTrigger;
+    PlotConditioned plotConditioned;
+
+    void Awake()
+    {
+        plotConditioned = this.gameObject.GetComponent<PlotConditioned>();
+    }
 
     void Start()
     {
+        this.gameObject.SetActive(CheckExistance());
         characterAnimator = this.gameObject.GetComponentInChildren<Animator>();
         characterAnimator.SetTrigger(startPosition.ToString());
         currentTrigger = startPosition.ToString();
+    }
+
+    private bool CheckExistance()
+    {
+        if (plotConditioned != null)
+        {
+            return plotConditioned.CheckCondition();
+        }
+        return true;
     }
 
     public void ChangeCharacterPosition(PositionToLook position)
