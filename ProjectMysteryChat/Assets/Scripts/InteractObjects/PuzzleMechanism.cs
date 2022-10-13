e1e8e4680dbe90f4f609a1bd657fb4cca7b9201b
Hollowriter <rodrigodiazklipphan@gmail.com>
2022-10-13 17:04:18 -0300

    Number mechanism working. Input limiter required.

---
+++ ProjectMysteryChat/Assets/Scripts/InteractObjects/PuzzleMechanism.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleMechanism : InteractObject
+{
+    [SerializeField]
+    Number number;
+
+    public override void BehaveInteraction()
+    {
+        number.IncreaseValue();
+        Debug.Log("NUMBER " + number.GetValue());
+    }
+}
