e7d163f225fa18324259d184878b1b1719ed7044
Hollowriter <rodrigodiazklipphan@gmail.com>
2023-06-11 14:18:24 -0300

    Making answers exclusive to debates

---
+++ ProjectMysteryChat/Assets/Scripts/ParseObjects/AnswerDebate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class AnswerDebate
+{
+    public string ExpectedAnswer;
+    public int ExpectedIndex;
+    public string CorrectDialog;
+    public string WrongDialog;
+}
+
+[System.Serializable]
+public class AnswerDebateCollection
+{
+    public AnswerDebate[] AnswersDebate;
+}
