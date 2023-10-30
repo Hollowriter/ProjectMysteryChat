0af49b2cea7643a88dc38f87ac61d32594f16bd2
Hollowriter <rodrigodiazklipphan@gmail.com>
2023-10-30 17:46:08 -0300

    Prologue, instructions and some visual references added

---
+++ ProjectMysteryChat/Assets/Scripts/Puzzles/NumberDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TextCore;
+
+public class NumberDisplay : MonoBehaviour
+{
+    [SerializeField] private TextMesh numberText;
+    [SerializeField] private Number number;
+
+    private void DisplayNumberValue() 
+    {
+        if (number != null && numberText != null)
+            numberText.text = number.GetValue().ToString();
+    }
+
+    private void Update()
+    {
+        DisplayNumberValue();
+    }
+}
---
+++ ProjectMysteryChat/Assets/Scripts/SingletonObjects/UIObjects/Instructions.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instructions : SingletonBase<Instructions>
+{
+    [SerializeField] private List<GameObject> instructions;
+
+    private void Awake()
+    {
+        SingletonAwake();
+    }
+
+    private void ObjectBehaviours() 
+    {
+        if (LevelManager.instance.GetSceneName() != "Menu" && LevelManager.instance.GetSceneName() != "Prologue") 
+        {
+            for (int i = 0; i < instructions.Count; i++) 
+            {
+                instructions[i].SetActive(true);
+            }
+        }
+        else 
+        {
+            for (int i = 0; i < instructions.Count; i++)
+            {
+                instructions[i].SetActive(false);
+            }
+        }
+    }
+
+    protected override void BehaveSingleton()
+    {
+        ObjectBehaviours();
+    }
+
+    private void Update()
+    {
+        BehaveSingleton();
+    }
+}
