e3e574c4fd487f2dabff4546933feb06d64f64c0
Hollowriter <deadgamergx2@hotmail.com.ar>
2024-02-08 23:28:39 -0300

    Music player with music added. Pending to add music to the dialogs.

--- ProjectMysteryChat/Assets/Scripts/SingletonObjects/Managers/MusicPlayer.cs
+++ ProjectMysteryChat/Assets/Scripts/SingletonObjects/Managers/MusicPlayer.cs
@@ -19,6 +19,8 @@ public enum MusicState
 public class MusicPlayer : SingletonBase<MusicPlayer>
 {
     private MusicState currentMusicState;
+    const string path = "Audio/Music/";
+    [SerializeField] private AudioSource musicSource;
 
     protected override void SingletonAwake()
     {
@@ -30,10 +32,46 @@ public class MusicPlayer : SingletonBase<MusicPlayer>
     private void Awake() 
     {
         SingletonAwake();
+        PlayMusic();
     }
 
-    private void PlayMusic() 
+    private void PlayMusic()
     {
+        switch (currentMusicState) 
+        {
+            case MusicState.INTRO:
+                musicSource.clip = Resources.Load<AudioClip>(path + "Cuadro de dialogo");
+                break;
+            case MusicState.INVESTIGATION:
+                musicSource.clip = Resources.Load<AudioClip>(path + "Investigacion");
+                break;
+            case MusicState.TENSION:
+                musicSource.clip = Resources.Load<AudioClip>(path + "Tension");
+                break;
+            case MusicState.MAINTHEME:
+                musicSource.clip = Resources.Load<AudioClip>(path + "Inicio");
+                break;
+            case MusicState.QUESTIONING:
+                musicSource.clip = Resources.Load<AudioClip>(path + "Interrogatorio");
+                break;
+            case MusicState.EXPLANATION:
+                musicSource.clip = Resources.Load<AudioClip>(path + "Explicacion");
+                break;
+            case MusicState.MSMAKI95:
+                musicSource.clip = Resources.Load<AudioClip>(path + "MsMaki95");
+                break;
+            case MusicState.CHRONOWRITER1885:
+                musicSource.clip = Resources.Load<AudioClip>(path + "ChronoWriter1885");
+                break;
+        }
+        if (currentMusicState == MusicState.NONE)
+        {
+            musicSource.Stop();
+        }
+        else
+        {
+            musicSource.Play();
+        }
     }
 
     public void SetMusicState(MusicState newMusicState) 
