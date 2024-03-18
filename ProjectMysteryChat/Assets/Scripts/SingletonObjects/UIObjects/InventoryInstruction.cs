012b6009c481fdfa49b68418f9beab2984417421
Hollowriter <deadgamergx2@hotmail.com.ar>
2024-03-18 16:55:38 -0300

    New scripts for the instruction elements, pending to be implemented

---
+++ ProjectMysteryChat/Assets/Scripts/SingletonObjects/UIObjects/InventoryInstruction.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InventoryInstruction : SingletonBase<InventoryInstruction>
+{
+    [SerializeField] private List<GameObject> elements;
+    [SerializeField] private GameObject notedElement;
+    public Action activateNotedElement;
+    public Action deactivateNotedElement;
+
+    protected override void SingletonAwake()
+    {
+        base.SingletonAwake();
+        SceneManager.activeSceneChanged -= ActivateDeactivateAll;
+        SceneManager.activeSceneChanged += ActivateDeactivateAll;
+        activateNotedElement -= ActivateNotedElement;
+        activateNotedElement += ActivateNotedElement;
+        deactivateNotedElement -= DeactivateNotedElement;
+        deactivateNotedElement += DeactivateNotedElement;
+    }
+
+    private void ActivateDeactivateAll(Scene previousScene, Scene nextScene)
+    {
+        if (nextScene.name != "Menu" && nextScene.name != "Prologue")
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                elements[i].SetActive(true);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < elements.Count; i++)
+            {
+                elements[i].SetActive(false);
+            }
+        }
+        notedElement.SetActive(false);
+    }
+
+    private void ActivateNotedElement() 
+    {
+        notedElement.SetActive(true);
+    }
+
+    private void DeactivateNotedElement() 
+    {
+        notedElement.SetActive(false);
+    }
+}
---
+++ ProjectMysteryChat/Assets/Scripts/SingletonObjects/UIObjects/SpaceInstruction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressSpace : SingletonBase<PressSpace>
+{
+    [SerializeField] private List<GameObject> elements;
+    public Action activateElements;
+    public Action deactivateElements;
+
+    protected override void SingletonAwake()
+    {
+        base.SingletonAwake();
+        activateElements += ActivateElements;
+        deactivateElements += DeactivateElements;
+    }
+
+    private void Awake() 
+    {
+        SingletonAwake();
+    }
+
+    private void ActivateElements() 
+    {
+        for (int i = 0; i < elements.Count; i++)
+        {
+            elements[i].SetActive(true);
+        }
+    }
+
+    private void DeactivateElements() 
+    {
+        for (int i = 0; i < elements.Count; i++)
+        {
+            elements[i].SetActive(false);
+        }
+    }
+}
